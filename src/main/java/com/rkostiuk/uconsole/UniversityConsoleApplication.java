4dcdfed2edfc3c73c7eb04a8eebc1745677e52f0
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-11-19 00:50:07 +0200

    Init

---
+++ src/main/java/com/rkostiuk/uconsole/UniversityConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.rkostiuk.uconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityConsoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rkostiuk/uconsole/UniversityConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkostiuk.uconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
