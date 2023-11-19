3370e8387a6a1e64f57d4dfbc376ab16e5c8bcde
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-11-19 23:24:31 +0200

    Rename CommandParserChain

--- src/main/java/com/rkostiuk/uconsole/command/parser/chain/CommandParserChain.java
+++ src/main/java/com/rkostiuk/uconsole/command/parser/chain/CommandParserChain.java
@@ -4,20 +4,17 @@ import com.rkostiuk.uconsole.command.CommandParam;
 import com.rkostiuk.uconsole.command.exception.UnknownCommandException;
 import com.rkostiuk.uconsole.command.parser.CommandParser;
 
-public abstract class AbstractCommandParserChain implements CommandParser {
-    private CommandParser fallback;
-
-    protected AbstractCommandParserChain() {
-    }
-
-    protected AbstractCommandParserChain(CommandParser fallback) {
-        this.fallback = fallback;
-    }
+public abstract class CommandParserChain implements CommandParser {
+    private CommandParserChain fallback;
 
     protected CommandParam callFallbackIfPresentOrElseThrow(String command) {
         if (fallback != null) {
             return fallback.parse(command);
         }
         throw new UnknownCommandException("Unknown command: '" + command + "'");
+    }
+
+    public void setFallback(CommandParserChain fallback) {
+        this.fallback = fallback;
     }
 }
