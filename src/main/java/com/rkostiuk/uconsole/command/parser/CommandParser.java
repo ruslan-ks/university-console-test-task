2d0b44cdc10ac7850e2494c4f77da1ab58649563
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-11-20 11:40:23 +0200

    Add exception to CommandParser#parse(String) signature

--- src/main/java/com/rkostiuk/uconsole/command/parser/CommandParser.java
+++ src/main/java/com/rkostiuk/uconsole/command/parser/CommandParser.java
@@ -1,7 +1,8 @@
 package com.rkostiuk.uconsole.command.parser;
 
 import com.rkostiuk.uconsole.command.CommandDetails;
+import com.rkostiuk.uconsole.command.exception.UnknownCommandException;
 
 public interface CommandParser {
-    CommandDetails parse(String command);
+    CommandDetails parse(String command) throws UnknownCommandException;
 }
